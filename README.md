30f6c0d373ed58f5e4e34dcc44d420b6986706a1
Jonatan Sombié <jonatan.sombie@gmail.com>
2022-07-29 17:03:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# auth0_cli
+
+DReAM package for creating auth0 cli client application configured for Device Authorization Flow.
---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+#  backend "s3" {}
+
+  required_providers {
+    random = {
+      source  = "registry.terraform.io/hashicorp/random"
+      version = "3.2.0"
+    }
+
+    auth0 = {
+      source  = "registry.terraform.io/auth0/auth0"
+      version = "0.32.0"
+    }
+  }
+}
+
+provider "random" {}
+provider "auth0" {}
+
+resource "random_pet" "client_name" {}
+
+resource "auth0_client" "client" {
+  name           = random_pet.client_name.id
+  description    = "Client configured for device authorization flow"
+  app_type       = "native"
+  is_first_party = true
+  jwt_configuration {
+    alg = "RS256"
+  }
+  grant_types     = ["urn:ietf:params:oauth:grant-type:device_code", "refresh_token"]
+  oidc_conformant = true
+  token_endpoint_auth_method = "none"
+}
+
+output "AUTH0_CLIENT_ID" {
+  sensitive = true
+  value     = auth0_client.client.client_id
+}
+
+data "auth0_tenant" "current" {}
+
+output "AUTH0_ISSUER_BASE_URL" {
+  sensitive = true
+  value     = "https://${data.auth0_tenant.current.domain}"
+}
